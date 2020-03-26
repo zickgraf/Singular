2fa4b96de545f4dbc27749949868b6793b433104
Fabian Zickgraf <f.zickgraf@dashdos.com>
2020-03-26 16:36:43 +0100

    Do not change posInT

--- kernel/GBEngine/kutil.cc
+++ kernel/GBEngine/kutil.cc
@@ -10186,7 +10186,7 @@ void initBuchMoraPos (kStrategy strat)
         strat->posInT = posInT15;
       else
         strat->posInT = posInT_EcartpLength;
-      strat->posInT = posInT15;
+      //strat->posInT = posInT15;
     }
     else if (currRing->pLexOrder && !TEST_OPT_INTSTRATEGY)
     {
